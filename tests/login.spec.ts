dea239fd63625d92f5a0177c73bb776971bb3e73
Jozxi <jsaracho1@gmail.com>
2026-01-03 20:02:11 -0300

    feat: implement Playwright login tests with a new page object and environment configuration.

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -4,8 +4,8 @@ test('login OK', async ({ page }) => {
     await page.goto('https://arky-seven.vercel.app/login');
 
 
-    await page.locator('#email').fill('tyson@test.com');
-    await page.locator('#password').fill('80ce8f7b23');
+    await page.locator('#email').fill('');
+    await page.locator('#password').fill('');
     await page.locator('button[type="submit"]').click();
 
     await expect(page).toHaveURL('https://arky-seven.vercel.app/dashboard')
@@ -15,7 +15,7 @@ test('login Fail', async ({ page }) => {
     await page.goto('https://arky-seven.vercel.app/login');
 
 
-    await page.locator('#email').fill('tyson@test.com');
+    await page.locator('#email').fill('');
     await page.locator('#password').fill('pepe1234');
     await page.locator('button[type="submit"]').click();
 
